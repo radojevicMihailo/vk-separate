370b8c8f050f100798bf5b5e138a9244b111a1ad
radojevicMihailo <radojevic.mihailo.00@gmail.com>
2025-02-06 12:13:30 +0100

    create bin file with processed vk

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-vk-separate
+# vk-separate
+
+run with command: 
+
+``` json_processor <json_vk_file_route> <route_output_files> <output_file_name>```
+
+example:
+
+``` json_processor vk_spark.json ./ processed_vk```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::fs::{self, File};
-use std::io::{self, Read};
+use std::io::Read;
 use clap::Parser;
+use nova_snark::provider::{PallasEngine, VestaEngine};
+use nova_snark::traits::circuit::{GenericCircuit, StepCircuit};
+use nova_snark::traits::evaluation::EvaluationEngineTrait;
+use nova_snark::traits::Engine;
+use nova_snark::VerifierKey;
+use pasta_curves::{Fp, Fq};
 use serde_json::{Value, Map, json};
 use serde_json::ser::{Serializer, PrettyFormatter};
+
+type EE<E> = nova_snark::provider::ipa_pc::EvaluationEngine<E>;
+type S<E, EE> = nova_snark::spartan::ppsnark::RelaxedR1CSSNARK<E, EE>;
 
 /// CLI tool for processing JSON files and removing specific fields.
 #[derive(Parser)]
@@ -18,7 +27,7 @@ struct Cli {
     output_file: String,
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     
     let mut file = File::open(&args.input_file)?;
@@ -51,7 +60,7 @@ fn main() -> io::Result<()> {
     
     // Ensure the output directory exists
     fs::create_dir_all(&args.output_dir)?;
-    let output_path = format!("{}/{}", args.output_dir, args.output_file);
+    let output_path = format!("{}/{}.json", args.output_dir, args.output_file);
     let mut output_file = File::create(&output_path)?;
     
     // Write formatted JSON output
@@ -59,7 +68,48 @@ fn main() -> io::Result<()> {
     let mut serializer = Serializer::with_formatter(&mut output_file, formatter);
     serde::Serialize::serialize(&json_value, &mut serializer)?;
     
-    println!("Successfully processed JSON file and removed 'ck' fields while preserving field order.");
+    println!("Successfully processed JSON file and replaced 'ck' value with empty array.");
+
+    fn create_bin_file<E1, E2, C1, C2, EE1, EE2>(
+        path: &str,
+        file_name: &str,
+    ) -> Result<Vec<u8>, Box<dyn std::error::Error>>
+    where
+        E1: Engine<Base = <E2 as Engine>::Scalar>,
+        E2: Engine<Base = <E1 as Engine>::Scalar>,
+        C1: StepCircuit<E1::Scalar>,
+        C2: StepCircuit<E2::Scalar>,
+        EE1: EvaluationEngineTrait<E1>,
+        EE2: EvaluationEngineTrait<E2>, 
+    {
+        let json_path_vk = format!("{}/{}.json", path, file_name);
+        // ! Read from JSON to String
+        let json_string_vk = fs::read_to_string(json_path_vk)?;
+
+        // ! From string into Vk
+        let json_data_vk: VerifierKey<E1, E2, C1, C2, S<E1, EE1>, S<E2, EE2>> =
+            serde_json::from_str(&json_string_vk)?;
+
+        // ! Serialize into Bytes
+        let bytes_vk = postcard::to_allocvec(&json_data_vk)?;
+
+        // ! Write bytes to BIN file
+        let output_bin_path_vk = format!("{}/{}", path, file_name);
+        fs::write(output_bin_path_vk.clone(), &bytes_vk)?;
+
+        Ok(bytes_vk)
+    }
+
+    let _ = create_bin_file::<
+        PallasEngine,
+        VestaEngine,
+        GenericCircuit<Fq>,
+        GenericCircuit<Fp>,
+        EE<_>,
+        EE<_>
+    >(&args.output_dir, &args.output_file)?;
+
+    println!("Successfully created binary file from JSON file after replacement.");
     
     Ok(())
 }
